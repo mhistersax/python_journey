0e77c1e5744fe630304e8c8ae7e7049aeea9d578
Mhistersax <bamigboyepelumi38@gmail.com>
2023-07-12 23:09:15 +0100

    simple arithmetic

---
+++ basic_python/intro.py
@@ -0,0 +1,10 @@
+'''This block of code save a particular value and add it up'''
+num1 = 5
+num2 = 5
+result = num1 + num2
+print("The result of this cal =", result)
+
+# this block get input from user and add it up
+num_to_get = int(input("Enter the first number: "))
+num_to_get2 = int(input("Enter second number: "))
+print("result of this operation", num_to_get + num_to_get2)
