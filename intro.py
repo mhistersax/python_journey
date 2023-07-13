eddc1b86450a6f3f92d7def9919f637e6a1f9651
Mhistersax <bamigboyepelumi38@gmail.com>
2023-07-13 21:09:22 +0100

    cantination Practice

--- intro.py
+++ intro.py
@@ -1,1 +1,6 @@
-print("hello world")
+capitalized_twi = "Twinkle,"
+lowercase_twi = " twinkle,"
+general_result = capitalized_twi + lowercase_twi + \
+    "little star, \n\tHow I wonder what you are!\n\t\tUp above the world do high,\n\t\tlike a diamond in the sky.\n" + \
+    capitalized_twi + lowercase_twi + "little star,\n\tHow I wonder what you are!"
+print(general_result)
