80b14bb0464de30986ff988eb98e8c7f563d8adc
Rihha <bamigboyepelumi38@gmail.com>
2023-07-15 01:38:12 +0100

    Completed day two

--- python/python-version.py
+++ python/python-version.py
@@ -1,3 +1,4 @@
+# @Author Oluwapelumi
 import sys
 print("Your Python version is")
 print(sys.version)
---
+++ python/reverse-namer.py
@@ -0,0 +1,9 @@
+# @Author Oluwapelumi
+#getting user first-name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+#reverse 
+print("Reversed value")
+print("Reversed values:")
+print(first_name[::-1])
+print(last_name[::-1])
