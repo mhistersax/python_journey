48a4be81aeb6b67c92ac9251af92faf6282f3655
Rihha <bamigboyepelumi38@gmail.com>
2023-07-15 02:27:57 +0100

    comma_numbers_converter

---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+# @Author Oluwapelumi
+# Getting user values from the user
+user_values = input("Enter Series of Value ")
+print("Printing your value in list ")
+list = user_values.split(",")
+print(list)
+
+# print(user_values.split(","))
